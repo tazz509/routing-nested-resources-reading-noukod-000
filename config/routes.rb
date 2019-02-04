292cf21b6bcc937acf21f697c73e672efc3e36e4
carly <carly@noukod.com>
2019-02-04 15:20:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
- 
+
   resources :authors, only: [:show] do
     # nested resource for posts
     resources :posts, only: [:show, :index]
   end
- 
+
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
- 
+
   root 'posts#index'
 end
